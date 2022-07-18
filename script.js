9118f6177f625087edd4f451e5a9a644617e364b
Antanas Pužas <antanaspuzas@protonmail.com>
2022-07-19 02:30:22 +0300

    Add event listener for square to display player choice

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 const Gameboard = (() => {
     const gameboard = [];
-    for (let i = 0; i < 9; i++) { gameboard.push("x") };
+    for (let i = 0; i < 9; i++) { gameboard.push(null) };
 
     const display = () => {
         const gameboardDiv = document.querySelector(".gameboard");
-        gameboard.forEach(el => {
+        gameboard.forEach((el, index) => {
             const square = document.createElement("div");
             square.classList.add("square");
             square.textContent = el;
+            square.id = "square-" + index;
+            square.addEventListener("click", () => _placeMarker(square, index));
             gameboardDiv.appendChild(square);
         })
     }
-    return { 
+
+    const _placeMarker = (square, index) => {
+        if (square.textContent === "") {
+            square.textContent = "X";
+            gameboard[index] = "X";
+            console.log(gameboard);
+        }
+    }
+
+    return {
         gameboard,
-        display, 
+        display,
     };
 })();
 
